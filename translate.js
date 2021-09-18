543776b4e02c549f03a1fd7e38d1d348433d44af
Francisco <public@francisco.io>
2021-09-18 15:00:01 +0900

    Added support for deepl

--- translate.js
+++ translate.js
@@ -385,7 +385,28 @@ const libre = {
     })
 };
 
-var engines = { google, yandex, libre };
+const deepl = {
+  needkey: true,
+  fetch: ({ key, from, to, text }) => {
+    const suffix = /:fx$/.test(key) ? "-free" : "";
+    text = encodeURIComponent(text);
+    return [
+      `https://api${suffix}.deepl.com/v2/translate?auth_key=${key}&source_lang=${from}&target_lang=${to}&text=${text}`,
+      { method: "POST", body: "" }
+    ];
+  },
+  parse: async res => {
+    if (!res.ok) {
+      if (res.status === 403) {
+        throw new Error("Auth Error, please review the key for DeepL");
+      }
+      throw new Error(`Error ${res.status}`);
+    }
+    return res.json().then(body => body.translations[0].text);
+  }
+};
+
+var engines = { google, yandex, libre, deepl };
 
 // From https://www.npmjs.com/package/memory-cache (Rollup didn't want to bundle it otherwise)
 'use strict';
