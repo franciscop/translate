5a1eb98ca730e02f0e1b2390eca9916fb10c1076
Mime <74558118+Mimexe@users.noreply.github.com>
2023-02-08 17:01:47 +0100

    Update index.d.ts
    optional
    successfully tested

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,16 @@
 declare module "translate" {
   function translate(
     text: string,
-    opts: {
-      from: string;
-      to: string;
-      cache: undefined;
-      languages: Function;
-      engines: Function;
-      engine: "google" | "deepl" | "libre" | "yandex";
-      keys: object;
+    opts?: {
+      from?: string;
+      to?: string;
+      cache?: undefined;
+      languages?: Function;
+      engines?: Function;
+      engine?: "google" | "deepl" | "libre" | "yandex";
+      keys?: object;
     }
   ): Promise<any>;
-  
+
   export = translate;
 }
