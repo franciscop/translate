8d0a0d8ac2ce92685bf6ce3c485321ab59eee7f3
Mime <74558118+Mimexe@users.noreply.github.com>
2023-02-08 16:53:33 +0100

    Create index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+declare module "translate" {
+  function translate(
+    text: string,
+    opts: {
+      from: string;
+      to: string;
+      cache: undefined;
+      languages: Function;
+      engines: Function;
+      engine: "google" | "deepl" | "libre" | "yandex";
+      keys: object;
+    }
+  ): Promise<any>;
+}
